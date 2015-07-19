3edb76bd4c307611ae5c394aa55d8ff9a280efe0
morganherlocker <morgan.herlocker@gmail.com>
2015-07-18 23:25:49 -0400

    music hall feed

---
+++ sources/music-hall.js
@@ -0,0 +1,23 @@
+var cheerio = require('cheerio')
+  , request = require('request')
+  , url = 'http://charlestonmusichall.com/events/'
+  , shows = []
+
+function musicfarm (done) {
+  request(url, function(err, response, body) {
+    var $ = cheerio.load(body)
+    $('.event_wrap').each(function(i, elem) {
+      var $$ = cheerio.load(elem)
+
+      var show = {
+        description: $$('.event_title').text().trim(),
+        url: $$('.event_title').children().attr('href'),
+        date: ($$('.month').text().trim() +' '+ $$('.day').text().trim() +' '+ $$('.weekday').text().trim() +' ' + $$('.venue_notes').text().trim()).split('\n').join(' ').split('\r').join(' ')
+      }
+      shows.push(show)
+    })
+    done(null, shows)
+  })
+}
+
+module.exports = musicfarm
