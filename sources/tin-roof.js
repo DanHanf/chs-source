8b8b686241109e471fb4e0e094e1ba44c57e93b1
Daniel Hanf <danhanf@gmail.com>
2015-07-18 21:39:45 -0400

    tin roof

---
+++ sources/tin-roof.js
@@ -0,0 +1,9 @@
+var cheerio = require('cheerio')
+  , request = require('request')
+  , url = 'https://www.reverbnation.com/venue/tinroofwashley'
+
+module.exports = function() {
+  request(url, function(err, response, body) {
+
+  })
+}
