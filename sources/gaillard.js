521bd6d8bc9fabc6ca8967c953d42243ebe82b36
Daniel Hanf <danhanf@gmail.com>
2015-07-21 00:51:59 -0400

    gaillard date formatting

--- sources/gaillard.js
+++ sources/gaillard.js
@@ -8,16 +8,28 @@ gaillard = function(done) {
     var $ = cheerio.load(body)
     $('.event-promo').each(function(i, elem) {
       var $$ = cheerio.load(elem)
+      title = $$('div.name').text().trim()
+      var date = $$('div.date').text().trim().split('Multiple Dates').join('').split('.').join('/')
+      date = normalizeDate(title, date)
       var show = {
         venue: 'Gaillard Center',
-        title: $$('div.name').text().trim(),
+        title: title,
         url: 'http://www.gaillardcenter.com'+$$('div.overlay').children().attr('href'),
-        date: $$('div.date').text().trim().split('Multiple Dates').join('').split('.').join('/')
+        date: date 
       }
       shows.push(show)
     })
     done(null, shows)
   })
+}
+
+normalizeDate = function(title, date) {
+  date = date.split('/')
+  var month = date.shift()
+  var day = date.shift()
+  date.push(month)
+  date.push(day)
+  return date.join('-') + '!Gaillard!' + title.trim()
 }
 
 module.exports = gaillard
