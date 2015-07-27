5d039470e0ff1aa106c8273960e44b58996f83b6
Daniel Hanf <danhanf@gmail.com>
2015-07-27 14:30:32 -0400

    cron runs scrapers/updates DB every 30 min

--- index.js
+++ index.js
@@ -4,28 +4,9 @@ var cronJob = require('cron').CronJob
 var show = require('./show.js')
 var generateHTML = require('./generateHTML.js')
 
-fs.readdir(__dirname+'/sources', function(err, dirs){
-  var q = queue(5)
-  dirs.forEach(function(dir){
-    var fn = require(__dirname + '/sources/' + dir)
-    q.defer(fn)
-  })
-  q.awaitAll(function(errs, results){
-    var shows = []
-    results.forEach(function(venue){
-      shows = shows.concat(venue)
-    })
-    show.getShows(function(result) {
-      generateHTML(result, function(tonight, thisWeek) {
-        console.log(tonight, thisWeek.join(' '))
-        // returns html for tonight and array of html for each venue this week
-      })
-    })
-    //console.log(JSON.stringify(shows))
-  })
-})
-
-var job = new cronJob('30 * * * *', function(){
+// Scrapers run / DB updated every 30 minutes
+new cronJob('*/30 * * * *', function(){
+  console.log('click')
   fs.readdir(__dirname+'/sources', function(err, dirs){
     var q = queue(5)
     dirs.forEach(function(dir){
@@ -37,10 +18,16 @@ var job = new cronJob('30 * * * *', function(){
       results.forEach(function(venue){
         shows = shows.concat(venue)
       })
+      console.log(shows)
+      show.addShows(shows)
     })
   })
-  }, function() {
-    console.log('done writing')
-  }, true
-)
-job.start()
+}, function(){console.log('written')}, true)
+
+
+show.getShows(function(result) {
+  generateHTML(result, function(tonight, thisWeek) {
+    //console.log(tonight, thisWeek.join(' '))
+    // returns html for tonight and array of html for each venue this week
+  })
+})
