c729162890c3b5128f1146d6ded47e6ee5312310
unfirthman <unfirthman@gmail.com>
2023-05-02 14:30:02 -0400

    chs-pour-house module now integrated css next and migrate to other venues

--- Python Rebuild/chs-source/src/App.jsx
+++ Python Rebuild/chs-source/src/App.jsx
@@ -1,38 +1,16 @@
+//App.jsx
 //import Calendar from './Components/Calendar/Calendar';
-import Table from './Components/Table/Table';
+//import Table from './Components/Table/Table';
+import './App.css'
+import ChsPourHouse from './Components/venueReact/chsPourHouse';
+
 
 const App = () => {
-  const data = [
-    { day: 'Sunday', event: 'Acoustic Jam', venue: 'The Blue Note' },
-    { day: 'Sunday', event: 'Open Mic Night', venue: 'The Roxy' },
-    { day: 'Monday', event: 'Karaoke Night', venue: 'The Karaoke Lounge' },
-    { day: 'Tuesday', event: 'Trivia Night', venue: 'The Pub' },
-    { day: 'Wednesday', event: 'Live Music', venue: 'The Loft' },
-    { day: 'Thursday', event: 'Comedy Night', venue: 'The Chuckle Hut' },
-    { day: 'Friday', event: 'DJ Night', venue: 'The Club' },
-    { day: 'Saturday', event: 'Live Band', venue: 'The Arena' },
-  ];
-
-  const filteredData = {};
-
-  data.forEach((item) => {
-    if (filteredData[item.day]) {
-      filteredData[item.day].push(item);
-    } else {
-      filteredData[item.day] = [item];
-    }
-  });
 
   return (
-    <div>
-      <h1>Events Calendar</h1>
-      {Object.entries(filteredData).map(([day, events]) => (
-        <div key={day}>
-          <h2>{`${day}'s Events:`}</h2>
-          <Table data={events} />
+        <div>
+           <ChsPourHouse />
         </div>
-      ))}
-    </div>
   );
 };
 
--- Python Rebuild/chs-source/src/Components/Table/Table.jsx
+++ Python Rebuild/chs-source/src/Components/Table/Table.jsx
@@ -1,37 +1,40 @@
-import "./Table.css";
-import PropTypes from "prop-types";
+import "./Table.css"
+import PropTypes from 'prop-types';
 
 const Table = ({ data }) => {
-    return (
-        <table>
-            <thead>
-                <tr>
-                    <th>Day</th>
-                    <th>Event</th>
-                    <th>Venue</th>
-                </tr>
-            </thead>
-            <tbody>
-                {data.map(({ day, event, venue }, index) => (
-                    <tr key={index}>
-                        {index % 3 === 0 && <td>{day}</td>}
-                        <td>{event}</td>
-                        <td>{venue}</td>
-                    </tr>
-                ))}
-            </tbody>
-        </table>
-    );
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>CHS PourHouse</th>
+          <th>Gaillard</th>
+          <th>Music Farm</th>
+          <th>Music Hall</th>
+          <th>Royal American</th>
+          <th>Theater 99</th>
+          <th>The WindJammer</th>
+        </tr>
+      </thead>
+      <tbody>
+        {data.map(({ day, event}, index) => (
+          <tr key={index}>
+            <td>{day}</td>
+            <td>{event}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
 };
 
 Table.propTypes = {
-    data: PropTypes.arrayOf(
-        PropTypes.shape({
-            day: PropTypes.string.isRequired,
-            event: PropTypes.string.isRequired,
-            venue: PropTypes.string.isRequired,
-        })
-    ).isRequired,
+  data: PropTypes.arrayOf(
+    PropTypes.shape({
+      day: PropTypes.string.isRequired,
+      event: PropTypes.string.isRequired,
+      venue: PropTypes.string.isRequired,
+    })
+  ).isRequired,
 };
 
 export default Table;
